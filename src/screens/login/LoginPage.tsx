62ff84b20f3ad687df4c3135b1e72afac8689596
Dorian Longépée <dorianlongepee.pro@gmail.com>
2023-05-22 21:39:00 +0200

    Project boilerplate after CRA

---
+++ src/screens/login/LoginPage.tsx
@@ -0,0 +1,9 @@
+import { Box, Typography } from "@mui/material";
+
+const LoginPage = () => (
+    <Box>
+        <Typography sx={{color:'red'}}>Hello world</Typography>
+    </Box>
+)
+
+export default LoginPage
